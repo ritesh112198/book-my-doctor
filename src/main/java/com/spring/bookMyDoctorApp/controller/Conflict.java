59e0d24d0bf17d37ba8a1dd35bda22d9092dce67
ritesh112198 <rai000257gmail.com>
2025-06-23 14:41:38 +0530

    conflict push

---
+++ src/main/java/com/spring/bookMyDoctorApp/controller/Conflict.java
@@ -0,0 +1,10 @@
+package com.spring.bookMyDoctorApp.controller;
+
+import java.sql.SQLOutput;
+
+public class Conflict {
+    public static void main(String[] args) {
+        System.out.println("next Conflict");
+    }
+
+}
