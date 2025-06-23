d05de63d94f0d648ffd0f3aa78ef43e08461a8ab
ritesh112198 <rai000257gmail.com>
2025-06-23 14:50:29 +0530

    conflict push

---
+++ src/main/java/com/spring/bookMyDoctorApp/controller/NextClass.java
@@ -0,0 +1,9 @@
+package com.spring.bookMyDoctorApp.controller;
+
+public class NextClass {
+
+    public static void main(String[] args) {
+        System.out.println("Conflict branch is added");
+    }
+
+}
